d087d1c98df21b49d918c9eecd96e70f823be133
Ricardo Matos <ricardothadeu8@outlook.com>
2022-03-30 01:05:49 -0300

    Add 'Renew loan' permission

--- catalog/models.py
+++ catalog/models.py
@@ -91,7 +91,8 @@ class BookInstance(models.Model):
     
     class Meta:
         ordering = ['due_back']
-        permissions = (("can_mark_returned", "Set book as returned"),)
+        permissions = (("can_mark_returned", "Set book as returned"),
+                      ("can_renew", "Renew a loan"),)
 
     def __str__(self):
         """String for representing the Model object"""
